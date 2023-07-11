8e86bc072b615140456fe0f7d2c468617239d2c5
Marco Perez <marco.perez@satinel-system.com>
2023-07-11 21:34:13 +0200

    @Input @Output Emitter

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+
+
+  @Input()
+  public characterList: Character[] = [{
+    name:'Trunk',
+    power: 10
+  }]
+
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number):void {
+    console.log("Index: ", index);
+    this.onDelete.emit(index);
+  }
+
+
 }
+""
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent  {
 
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  },{
+    name: 'Goku',
+    power:9500
+    },{
+    name:'Vegeta',
+    power: 7500
+  }
 
+  ];
+
+  onNewCharacter1(character: Character):void {
+    console.log("Main page");
+    console.log(character);
+    // this.characters.unshift(character); //agrega al inicio
+    this.characters.push(character); //agrega al final
+  }
+
+  onDeleteCharacter(index: number):void {
+    // console.log(this.characters.splice);
+    this.characters.splice(index, 1);
+  }
 
 }
